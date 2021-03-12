484f9a556af49566c2e574aadc51c1a4cd67be47
nownabe <nownabe@gmail.com>
2021-03-12 21:01:51 +0900

    Initialize Next.js app

---
+++ ai-api/web/.eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        config: 'tsconfig.json',
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import/order': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+      },
+    ],
+    'react/prop-types': ['off'],
+    'react/react-in-jsx-scope': 'off'
+  }
+};
+
