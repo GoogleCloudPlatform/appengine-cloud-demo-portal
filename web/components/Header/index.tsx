e1f51629cd9938b7a64b0464d576c5e9e3bd0d18
nownabe <nownabe@gmail.com>
2021-03-13 23:22:34 +0900

    Make title a link

--- web/components/Header/index.tsx
+++ web/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { GitHub } from "@material-ui/icons";
+import { useRouter } from "next/router";
 
 import { useTranslation } from "../../hooks/useTranslation";
 import LangMenu from "./LangMenu";
@@ -12,19 +13,27 @@ const useStyles = makeStyles((_theme: Theme) =>
     },
     title: {
       flexGrow: 1,
+      cursor: "pointer",
     },
   })
 );
 
 const GlobalNavigation: React.FC = () => {
+  const router = useRouter();
   const classes = useStyles();
-  const { t } = useTranslation();
+  const { t, locale } = useTranslation();
+
+  const onClickTitle = () => router.push("/", "/", { locale });
 
   return (
     <header>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography
+            variant="h6"
+            className={classes.title}
+            onClick={onClickTitle}
+          >
             {t.title}
           </Typography>
           <nav>
