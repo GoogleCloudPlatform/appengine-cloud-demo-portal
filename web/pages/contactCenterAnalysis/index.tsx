b01420d25ba9940597a1adb940c5cde3ce1743b3
nownabe <nownabe@gmail.com>
2021-03-15 09:09:46 +0900

    Show demo info

--- web/pages/contactCenterAnalysis/index.tsx
+++ web/pages/contactCenterAnalysis/index.tsx
@@ -1,4 +1,8 @@
-import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import { createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
+
+import ProductChips from "../../components/ProductChips";
+import { useTranslation } from "../../hooks/useTranslation";
+import { demos } from "../../src/demos";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,15 +16,35 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: "center",
       alignItems: "center",
     },
+    title: {
+      marginBottom: theme.spacing(2),
+    },
+    description: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(4),
+    },
   })
 );
 
+const demo = demos["contactCenterAnalysis"];
+
 const ContactCenterAnalysis: React.FC = () => {
   const classes = useStyles();
+  const { t } = useTranslation();
 
   return (
     <main className={classes.root}>
-      <h1>Contact Center Analysis</h1>
+      <Typography variant="h3" component="h2" className={classes.title}>
+        {t.contactCenterAnalysis.title}
+      </Typography>
+      <ProductChips productIds={demo.products} />
+      <Typography
+        variant="subtitle1"
+        component="p"
+        className={classes.description}
+      >
+        {t.contactCenterAnalysis.description}
+      </Typography>
     </main>
   );
 };
