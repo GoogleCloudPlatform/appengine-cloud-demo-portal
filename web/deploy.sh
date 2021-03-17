a36e010fa9806b36405420675eef8455b672f983
nownabe <nownabe@gmail.com>
2021-03-17 23:53:03 +0900

    Add deploy script

---
+++ web/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Set environment variables: API_HOST, API_USE_SSL
+
+npm run build
+
+gcloud app deploy --project "${GOOGLE_PROJECT}"
