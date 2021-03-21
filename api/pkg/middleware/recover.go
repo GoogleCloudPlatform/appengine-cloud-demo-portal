0fd45b49735757a57efaf8cf2613560c2cfcca9e
nownabe <nownabe@gmail.com>
2021-03-21 14:53:04 +0900

    Ignore http.ErrAbortHandler in recover

--- api/pkg/middleware/recover.go
+++ api/pkg/middleware/recover.go
@@ -11,7 +11,7 @@ func Recover() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
-				if rvr := recover(); rvr != nil {
+				if rvr := recover(); rvr != nil && rvr != http.ErrAbortHandler {
 					logger := log.Ctx(r.Context())
 					logger.Error().Msgf("recover from: %v", rvr)
 					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
