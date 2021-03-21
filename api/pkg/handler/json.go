253c9ca2a663fd85d8a48dbac3e0d5f655aab761
nownabe <nownabe@gmail.com>
2021-03-21 17:00:12 +0900

    Log error

--- api/pkg/handler/json.go
+++ api/pkg/handler/json.go
@@ -3,12 +3,18 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 // RespondJSON responds body as JSON.
 func RespondJSON(w http.ResponseWriter, r *http.Request, status int, body interface{}) {
+	ctx := r.Context()
+	logger := log.Ctx(ctx)
+
 	bytes, err := json.Marshal(body)
 	if err != nil {
+		logger.Error().Err(err).Msg("failed to marshal response body as json")
 		RespondError(w, r,
 			E(http.StatusInternalServerError, "internal server error",
 				"failed to marshal response body as a JSON: %w", err))
@@ -17,5 +23,6 @@ func RespondJSON(w http.ResponseWriter, r *http.Request, status int, body interf
 
 	w.WriteHeader(status)
 	if _, err := w.Write(bytes); err != nil {
+		logger.Error().Err(err).Msg("failed to write json response")
 	}
 }
