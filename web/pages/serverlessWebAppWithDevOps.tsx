927ee80362fce524b344eebecd8a814f123679ea
nownabe <nownabe@gmail.com>
2021-03-30 22:02:38 +0900

    :sparkles: [web] Add architecture of serverless web app with devops demo

--- web/pages/serverlessWebAppWithDevOps.tsx
+++ web/pages/serverlessWebAppWithDevOps.tsx
@@ -1,14 +1,33 @@
+import {
+  createStyles,
+  Grid,
+  makeStyles,
+  Paper,
+  Theme,
+} from "@material-ui/core";
 import Head from "next/head";
+import Image from "next/image";
 import DemoContainer from "../components/DemoContainer";
 import { useError } from "../hooks/useError";
 import { useTranslation } from "../hooks/useTranslation";
 import { demos } from "../src/demos";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    panel: {
+      padding: theme.spacing(4),
+    },
+    architecture: {},
+  })
+);
+
 const demo = demos["serverlessWebAppWithDevOps"];
 
 const ServerlessWebAppWithDevOps: React.FC = () => {
   const { t } = useTranslation();
+  const classes = useStyles();
   const { errorMessage, onCloseError } = useError();
+
   return (
     <DemoContainer
       title={t.serverlessWebAppWithDevOps.title}
@@ -23,6 +42,17 @@ const ServerlessWebAppWithDevOps: React.FC = () => {
           {t.serverlessWebAppWithDevOps.title} | {t.title}
         </title>
       </Head>
+      <Grid item>
+        <Paper className={classes.panel}>
+          <Image
+            src="/images/serverlessWebAppWithDevOps/architecture.png"
+            alt="Architecture of serverless web app with DevOps"
+            className={classes.architecture}
+            width={2017}
+            height={781}
+          />
+        </Paper>
+      </Grid>
     </DemoContainer>
   );
 };
